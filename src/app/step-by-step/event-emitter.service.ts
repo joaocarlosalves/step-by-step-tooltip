5c8d52d3a81060e1bf0f3959391d76b6835b991f
JOAO  CARLOS ALVES <klb.jcalves@sompo.com.br>
2020-02-10 11:19:41 -0300

    second commit

--- src/app/step-by-step/event-emitter.service.ts
+++ src/app/step-by-step/event-emitter.service.ts
@@ -1,15 +1,10 @@
 import { EventEmitter } from '@angular/core';
 
 export class EventEmitterService {
+    private static emitters: { [eventName: string]: EventEmitter<any> } = {}
 
-    private static emitters: {
-        [nomeEvento: string]: EventEmitter<any>
-    } = {}
-
-    static get (nomeEvento:string): EventEmitter<any> {
-        if (!this.emitters[nomeEvento])
-            this.emitters[nomeEvento] = new EventEmitter<any>();
-        return this.emitters[nomeEvento];
+    static get (eventName: string): EventEmitter<any> {
+        if (!this.emitters[eventName]) this.emitters[eventName] = new EventEmitter<any>()
+        return this.emitters[eventName]
     }
-
 }
